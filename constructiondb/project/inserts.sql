d4ba9e8d85fd215aab12058631a3e5d373bdc5cc
smi23098 <164540992+smi23098@users.noreply.github.com>
2024-07-03 09:55:48 -0600

    commit

--- constructiondb/project/inserts.sql
+++ constructiondb/project/inserts.sql
@@ -24,3 +24,31 @@ INSERT INTO zip_code (zip_code) VALUES
     ("84044"), -- Salt Lake
     ("84321"); -- Logan
     
+INSERT INTO address (city_id, street_num, street_name, unit_num, zip_code_id, state_id) VALUES
+	(
+        (SELECT city_id FROM city WHERE city_name = "Middleton"),
+        '121',
+        'Blazing Trail Dr.',
+        200,
+        (SELECT zip_code_id FROM city WHERE zip_code = "83644"),
+        (SELECT state_id FROM state WHERE state_name = "Idaho")
+    ), 
+    ((SELECT city_id FROM city WHERE city_name = "Salt Lake"),
+        '300',
+        'North Temple',
+        1230,
+        (SELECT zip_code_id FROM city WHERE zip_code = "84044"),
+        (SELECT state_id FROM state WHERE state_name = "Utah")
+        ),
+	((SELECT city_id FROM city WHERE city_name = "Rexburg"),
+	'400',
+	'2nd East',
+	100,
+	(SELECT zip_code_id FROM city WHERE zip_code = "83440"),
+	(SELECT state_id FROM state WHERE state_name = "Idaho"));
+    
+    INSERT INTO insurance_company (insurance_company_id, company_name, address_id) VALUES
+		(
+        
+        )
+    
