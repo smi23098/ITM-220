881cf4fa78ac27d8441c505a7bd74c7652362173
alexanderdombroski <dom21013@byui.edu>
2024-07-10 08:20:15 -0600

    finished address tables

---
+++ scripts/label-expander.js
@@ -0,0 +1,47 @@
+// mouseenter | mouseleave
+
+(function initForms() {
+    const forms = document.querySelectorAll('.address-fieldset');
+    const formPromises = Array.from(forms).map(form => addFieldset(form));
+    Promise.all(formPromises).then(() => {
+        console.log('listen');
+        initListeners(); // Initialize listeners after all forms are processed
+    });
+})();
+
+function addFieldset(form) {
+    return fetch('address-fields.html')
+        .then(response => response.text())
+        .then(data => {
+            form.innerHTML += data;
+            console.log('add')
+        });
+}
+
+function initListeners() {
+    document.querySelectorAll('label').forEach(activator => activator.addEventListener('click', showInfo));
+};
+
+const infoTable = document.getElementById("mysql-info");
+
+function showInfo(event) {
+    console.log(event.target);
+    infoTable.innerHTML = 
+        `<h3>Database Connection</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Table</th>
+                    <th>Column</th>
+                    <th>Foriegn Key</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${event.target.getAttribute('data-table')}</td>
+                    <td>${event.target.getAttribute('data-column')}</td>
+                    <td>${event.target.getAttribute('data-foriegn')}</td>
+                </tr>
+            </tbody>
+        </table`
+}
