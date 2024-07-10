87382021d7a315562bfe9db48e51aa2bc9a464ce
Ben-Klelik <bam.michaelis@gmail.com>
2024-07-10 10:04:08 -0600

    Added updates sql file

---
+++ constructiondb/project/updates.sql
@@ -0,0 +1,29 @@
+USE constructiondb;
+
+-- address
+
+-- state
+
+-- city
+
+-- zip code
+
+-- company
+
+-- insurance
+
+-- insurance company
+
+-- insurance type
+
+-- project
+
+-- template
+
+-- task
+
+-- subcontractor
+
+-- subcontractor task
+
+-- subcontractor address
