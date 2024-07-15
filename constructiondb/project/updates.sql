b99123523cec6734e7b0b1c908c8a2d5b21a3860
alexanderdombroski <dom21013@byui.edu>
2024-07-15 09:26:18 -0600

    subcontractor_address updates

--- constructiondb/project/updates.sql
+++ constructiondb/project/updates.sql
@@ -27,3 +27,20 @@ USE constructiondb;
 -- subcontractor task
 
 -- subcontractor address
+UPDATE subcontractor_address
+SET addr_end = '2024-7-10'
+WHERE subcontractor_id = (
+	SELECT subcontractor_id FROM subcontractor
+    WHERE subcontractor_name = "Jim's Construction"
+) AND address_id = (
+	SELECT address_id FROM address AS a
+    JOIN city AS c ON c.city_id = a.city_id
+    JOIN state AS s ON s.state_id = s.state_id
+    WHERE street_num = 980 
+		AND street_name = "Construction Way"	
+		AND unit_num = "56"
+		AND city_name = "Madison"
+        AND state_name = "Wisconsin"
+);
+
+SELECT * FROM subcontractor_address;
