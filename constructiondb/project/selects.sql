42ff0421b3ef2670c19452f2db1d4733507ffb0e
Ben-Klelik <bam.michaelis@gmail.com>
2024-07-19 10:02:50 -0600

    Added insurance select

--- constructiondb/project/selects.sql
+++ constructiondb/project/selects.sql
@@ -1,3 +1,5 @@
+USE constructiondb;
+
 -- Read select for Subcontractor
 
 SELECT subcontractor_name, street_num, street_name, city_name, addr_start, addr_end 
@@ -15,3 +17,13 @@ JOIN task AS T ON T.task_id = ST.task_id
 JOIN project AS P ON P.project_id = T.project_id
 JOIN address AS A ON A.address_id = P.address_id
 JOIN city AS C ON C.city_id = A.city_id;
+
+SELECT subcontractor_name, type_name, company_name, exp_date,
+(CASE
+    WHEN exp_date < curdate() THEN "Expired"
+    ELSE "Current"
+ END) AS "Expired"
+FROM insurance AS i
+JOIN subcontractor AS s ON s.subcontractor_id = i.subcontractor_id
+JOIN insurance_type AS it ON it.insurance_type_id = i.insurance_type_id
+JOIN insurance_company AS ic ON ic.insurance_company_id = i.insurance_company_id;
