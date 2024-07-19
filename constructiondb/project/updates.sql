7bf54d503ba4ea651498ac94304389319486ea39
alexanderdombroski <dom21013@byui.edu>
2024-07-18 21:20:07 -0600

    reorganized updates

--- constructiondb/project/updates.sql
+++ constructiondb/project/updates.sql
@@ -1,7 +1,5 @@
 USE constructiondb;
 
--- address
-
 -- state (correct the spelling of mississippi)
 
 UPDATE state AS s1
@@ -11,16 +9,26 @@ JOIN (
 ON s1.state_id = s2.state_id
 SET s1.state_name = 'Mississippi';
 
--- city
+-- Update Subcontractor Address to show they have moved their HQ
 
--- zip code
+UPDATE subcontractor_address
+SET addr_end = '2024-7-10'
+WHERE subcontractor_id = (
+	SELECT subcontractor_id FROM subcontractor
+    WHERE subcontractor_name = "Jim's Construction"
+) AND address_id = (
+	SELECT address_id FROM address AS a
+    JOIN city AS c ON c.city_id = a.city_id
+    JOIN state AS s ON s.state_id = s.state_id
+    WHERE street_num = 980 
+		AND street_name = "Construction Way"	
+		AND unit_num = "56"
+		AND city_name = "Madison"
+        AND state_name = "Wisconsin"
+);
 
--- company
-
--- insurance
 
 -- insurance company (name change)
--- SELECT company_name,street_num, street_name FROM insurance_company  AS i JOIN address AS a ON a.address_id = i.address_id;
 
 UPDATE insurance_company AS ic1
 JOIN (
@@ -37,31 +45,3 @@ JOIN (
 ON ic1.insurance_company_id = ic2.insurance_company_id
 SET company_name = "New Day Insurance";
 
--- insurance type
-
--- project
-
--- template
-
--- task
-
--- subcontractor
-
--- subcontractor task
-
--- subcontractor address
-UPDATE subcontractor_address
-SET addr_end = '2024-7-10'
-WHERE subcontractor_id = (
-	SELECT subcontractor_id FROM subcontractor
-    WHERE subcontractor_name = "Jim's Construction"
-) AND address_id = (
-	SELECT address_id FROM address AS a
-    JOIN city AS c ON c.city_id = a.city_id
-    JOIN state AS s ON s.state_id = s.state_id
-    WHERE street_num = 980 
-		AND street_name = "Construction Way"	
-		AND unit_num = "56"
-		AND city_name = "Madison"
-        AND state_name = "Wisconsin"
-);
